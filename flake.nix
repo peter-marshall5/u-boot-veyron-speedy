29a6e3d43d2b938564d268735745e842035cfe92
Peter Marshall <petms@proton.me>
2023-12-27 17:17:26 -0500

    Update u-boot version and slim config a bit

--- flake.nix
+++ flake.nix
@@ -8,20 +8,34 @@
       pkgs = (import nixpkgs) {
         system = "x86_64-linux";
       };
-      plat = pkgs.pkgsCross.armv7l-hf-multiplatform;
+      plat = pkgs.pkgsCross.arm-embedded;
+      ubootVersion = "2024.01-rc5";
+      ubootSrc = pkgs.fetchurl {
+        url = "https://ftp.denx.de/pub/u-boot/u-boot-${ubootVersion}.tar.bz2";
+        hash = "sha256-X8jsKGWHEYrCqOnqEvXhn4EFhHBSQopVGvG+dy4Xsc0=";
+      };
     in
     {
       bin.speedy = plat.buildUBoot {
+        version = ubootVersion;
+        src = ubootSrc;
         defconfig = "chromebook_speedy_defconfig";
-        extraMeta.platforms = [ "armv7l-linux" ];
         extraConfig = ''
           CONFIG_TEXT_BASE=0x02000100
           CONFIG_BOOTDELAY=-2
           CONFIG_BOOTCOMMAND="bootflow scan; reset"
           CONFIG_SKIP_LOWLEVEL_INIT=y
-          CONFIG_EFI_LOADER=y
+          CONFIG_BOARD_EARLY_INIT_R=n
           CONFIG_CONSOLE_TRUETYPE=n
+          CONFIG_LEGACY_IMAGE_FORMAT=n
+          CONFIG_BOOTSTD_DEFAULTS=n
+          CONFIG_NET=n
+          CONFIG_BOOTMETH_EFILOADER=y
+          CONFIG_VIDEO_LOGO=n
         '';
+        extraMeta.platforms = ["arm-none"];
+        patches = [];
+        extraMakeFlags = [ "DTC=${pkgs.dtc}/bin/dtc" ];
         filesToInstall = [ "u-boot.bin" "u-boot.dtb" ];
       };
       bin.speedy-kpart = pkgs.callPackage ./u-boot-kpart.nix {
